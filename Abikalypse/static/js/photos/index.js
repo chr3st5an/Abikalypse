5db88e965de408c690bc5f94ae660c99329fbafe
chr3st5an <christiankreutz03@protonmail.com>
2022-05-12 21:29:35 +0200

    Changed behaviour

--- Abikalypse/static/js/photos/index.js
+++ Abikalypse/static/js/photos/index.js
@@ -1,20 +1,31 @@
 window.onload = () => {
-    /* Load group photos */
-    {
-        for (let photo of document.querySelectorAll('.group-photos img')) {
-            photo.setAttribute('width', String(window.innerWidth / 1.2));
-            photo.setAttribute('height', 'auto');
-        }
+    for (let container of document.querySelectorAll('.img-container')) {
+        let photo = container.getElementsByTagName('img')[0];
+
+        photo.setAttribute('width', String(window.innerWidth / 1.2));
+        photo.setAttribute('height', 'auto');
+
+        container.addEventListener('mousedown', e => e.preventDefault() || false);
     }
 }
 
 window.onresize = () => {
-    /* Resize group photos */
-    {
-        const groupPhotos = document.querySelectorAll('.group-photos img');
+    /* Resize photos */
+    for (let photo of document.getElementsByTagName('img')) {
+        photo.setAttribute('width', String(window.innerWidth / 1.2));
+    }
+}
 
-        groupPhotos.forEach(img => {
-            img.setAttribute('width', String(window.innerWidth / 1.2));
-        })
+window.onscroll = () => {
+    if (window.innerWidth > 700) {
+        for (let container of document.querySelectorAll('.img-container')) {
+            let rect = container.getBoundingClientRect();
+
+            if (rect.top / window.innerHeight < 0.15 && rect.bottom / window.innerHeight > 0.15) {
+                container.focus({'preventScroll': true});
+            } else {
+                container.blur();
+            }
+        }
     }
 }
