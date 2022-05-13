53598488f6ecb1c2ea78da1a2f097a9f8139522d
chr3st5an <christiankreutz03@protonmail.com>
2022-05-13 18:47:00 +0200

    Update images & view behaviour

--- Abikalypse/static/js/photos/index.js
+++ Abikalypse/static/js/photos/index.js
@@ -9,7 +9,7 @@ window.onscroll = () => {
         for (let container of document.querySelectorAll('.img-container')) {
             let rect = container.getBoundingClientRect();
 
-            if (rect.top / window.innerHeight < 0.15 && rect.bottom / window.innerHeight > 0.15) {
+            if (rect.top / window.innerHeight < 0.2 && rect.bottom / window.innerHeight > 0.15) {
                 container.focus({'preventScroll': true});
             } else {
                 container.blur();
