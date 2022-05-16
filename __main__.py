0b6f76509b5f7f606c0d78cef83f971d36b3374f
chr3st5an <christiankreutz03@protonmail.com>
2022-05-16 13:07:43 +0200

    Start gunicorn

--- __main__.py
+++ __main__.py
@@ -1,7 +1,15 @@
-from aiohttp.web import run_app
+"""Start gunicorn in debugging mode
+"""
 
-from Abikalypse import app
+import sys
+import os
 
 
 if __name__ == '__main__':
-    run_app(app)
+    major, minor = sys.version_info[:2]
+
+    if major < 3 or minor < 8:
+        raise Exception("[module]: require Python 3.8 or higher")
+
+    os.chdir(os.path.realpath(os.path.dirname(__file__)))
+    os.system('gunicorn --reload')
